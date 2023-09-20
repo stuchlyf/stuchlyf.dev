98846c17b8dee2f9421c86d398416b0361b7df53
Florian Stuchly <stuchlyf@stuchlyf.dev>
2023-09-20 20:32:15 +0200

    Adjust text of Home-Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import avatar from '@/../public/avatar.png';
 import Image from 'next/image';
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className={'h-full p-16 flex justify-between items-center'}>
       <div className={'font-light'}>
-        <h1 className={'text-5xl font-bold mb-2'}>Hey, my name is Florian.</h1>
+        <h1 className={'text-5xl font-bold mb-2'}>Hey! My name is Florian.</h1>
         <p className={'text-5xl'}>I&#39;m a software engineer based in Hof.</p>
         <p className={'text-5xl'}>I like to build things based on bleeding edge technologies.</p>
       </div>
