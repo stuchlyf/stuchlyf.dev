14c90ea1c83c1df77eae721c6d8f629dcc3ae552
Florian Stuchly <florian.stuchly@accenture.com>
2023-09-21 09:10:44 +0200

    Update the project-schema

--- src/models/project.ts
+++ src/models/project.ts
@@ -3,8 +3,8 @@ import {z} from "zod";
 const projectSchema = z.object({
   name: z.string(),
   description: z.string(),
-  githubLink: z.string().url(),
-  link: z.string().url(),
+  githubLink: z.string().url().optional(),
+  link: z.string().url().optional(),
   image: z.string(),
 });
 
