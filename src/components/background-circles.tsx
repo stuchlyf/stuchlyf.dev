a9b7e30ac53ab170d49a741af95557d78361d3dc
Florian Stuchly <stuchlyf@stuchlyf.dev>
2024-06-14 21:59:26 +0200

    chore: delete everything

--- src/components/background-circles.tsx
+++
@@ -1,7 +0,0 @@
-export default function BackgroundCircles() {
-  return (
-    <div
-      className={'absolute top-0 left-0 h-full w-full overflow-hidden -z-50 bg-background-blobs'}
-    />
-  );
-};
